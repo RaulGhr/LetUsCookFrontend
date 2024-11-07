5d7a101e3ae3e9d72cefdcdf2c592a0a77442c66
Gherman Raul <raul.gherman2804@gmail.com>
2024-11-07 22:10:38 +0200

    (add) Landing Page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Routes, Route, Navigate} from 'react-router-dom';
 import './App.scss';
 
 import Navbar from './views/navbar/Navbar.view';
+import LandingPage from './views/landingPage/landingPage.view';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navbar/>}>
+        <Route path="/" element={<LandingPage/>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
       
---
+++ src/views/landingPage/landingPage.view.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link, useNavigate } from 'react-router-dom';
+
+import "./landingPage.style.scss";
+
+import HeroSectionImage from "../../assets/images/HeroSection.png";
+
+
+const LandingPage = () => {
+
+    const navigate = useNavigate();
+    return (
+        <div className="LandingPage">
+            <div className="leftPart">
+                <h1>Cook smart, eat balanced!</h1>
+                <p>Discover endless recipes, plan balanced meals, and <br /> simplify your grocery shopping—all in one app.</p>
+                <button onClick={navigate('register')}>Sign Up</button>
+            </div>
+            <div className="rightPart">
+                <img src={HeroSectionImage} alt="Hero Section" />
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;

2993a8483c9d27914f2433a14e20e59d63da1b67
Gherman Raul <raul.gherman2804@gmail.com>
2024-11-12 21:03:39 +0200

    (add) AddRecipePage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.scss';
 import Navbar from './views/navbar/Navbar.view';
 import LandingPage from './views/landingPage/landingPage.view';
 import ExplorePage from './views/explorePage/explorePage.view';
+import AddRecipePage from './views/addRecipePage/addRecipePage.view';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Navbar/>}>
         <Route path="/" element={<LandingPage/>} />
         <Route path="/explore" element={<ExplorePage/>} />
+        <Route path="/addRecipe" element={<AddRecipePage/>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
       

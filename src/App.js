e6f23badafba01f54ee0edac63e2e3827c3e7c64
Mark-Konrad Kristo <99960484+Amouon@users.noreply.github.com>
2024-11-14 18:00:32 +0200

    Hotfix: Add removed route back

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ExplorePage from './views/explorePage/explorePage.view';
 import AddRecipePage from './views/addRecipePage/addRecipePage.view';
 import Register from './views/authentification/Register.view';
 import Login from './views/authentification/Login.view';
+import ViewRecipePage from "./views/viewRecipePage/viewRecipePage.view";
 
 import { AuthProvider } from './contexts/authContext';
 
@@ -21,6 +22,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/addRecipe" element={<AddRecipePage />} />
+          <Route path="/explore/:id" element={<ViewRecipePage />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
